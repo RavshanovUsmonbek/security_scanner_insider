98ca4f0bce964b26a5f4e61317b2456ebadeeed1
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-03-14 12:24:54 +0500

    Fix Bug: integrations deletion

--- static/js/integrations.js
+++ static/js/integrations.js
@@ -192,7 +192,8 @@ const insiderIntegration = {
             }).then(response => {
                 this.is_fetching = false
                 if (response.ok) {
-                     this.$emit('update', {...this.$data, section_name: this.section_name})
+                    delete this.$data['id']
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
                 } else {
                     this.handleError(response)
                     alertMain.add(`Deletion error. <button class="btn btn-primary" @click="modal.modal('show')">Open modal<button>`)
